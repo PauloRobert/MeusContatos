8abf6b40fc538c4044692498aa014319a4a1846c
Paulo Roberto <evans_sp@hotmail.com>
2025-10-04 15:08:04 -0300

    Atualizar a lista sempre que voltar

--- app/src/main/java/com/example/meuscontatos/MainActivity.java
+++ app/src/main/java/com/example/meuscontatos/MainActivity.java
@@ -44,28 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadContatos(){
         ContatoDAO contatoDAO = new ContatoDAO(this);
-
-        Contato c1 = new Contato(1, "Contato 1");
-        Contato c2 = new Contato(2, "Contato 2");
-        Contato c3 = new Contato(3, "Contato 3");
-        contatoDAO.inserirContato(c1);
-        contatoDAO.inserirContato(c2);
-        contatoDAO.inserirContato(c3);
-
         List<Contato> contatos = contatoDAO.listarContatos();
-        //List<Contato> contatos = fakeContatos();
         ContatoAdapter contatoAdapter = new ContatoAdapter(contatos, this);
-
         ListView lvContatos = findViewById(R.id.lvContatos);
         lvContatos.setAdapter(contatoAdapter);
-    }
-
-    private List<Contato> fakeContatos(){
-        Contato c1 = new Contato(1, "Contato 1");
-        Contato c2 = new Contato(2, "Contato 2");
-        Contato c3 = new Contato(3, "Contato 3");
-
-        List<Contato> contatos = Arrays.asList(c1, c2, c3);
-        return contatos;
     }
 }
